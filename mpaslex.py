8c6db86f29d18926445863a08a0442ab9a4e3e0f
RafaPinzon <rafapinzon_@hotmail.com>
2014-08-22 10:37:35 -0500

    Corrección lexer

--- mpaslex.py
+++ mpaslex.py
@@ -23,15 +23,36 @@ reserved = {
     'return' : 'RETURN',
     }
 
-tokens = ['EQUALS','PLUS','MINUS','TIMES','DIVIDE','POWER',
-     'LPAREN','RPAREN','LT','LE','GT','GE','NE', 'NINT', 'NFLOAT'
-     'COMMA','SEMI','SEMICOLON', 'ASSIGN','' 'INTEGER','FLOAT', 'STRING',
-     'ID','NEWLINE'] + list(reserved.values())
+tokens = ['EQUALS','PLUS','MINUS','TIMES','DIVIDE','LPAREN',
+    'RPAREN','LT','LE','GT','GE','NE', 'NINT', 'NFLOAT'
+    'COMMA','SEMI','SEMICOLON', 'ASSIGN','INTEGER','FLOAT',
+    'STRING','ID','NEWLINE'] + list(reserved.values())
 
 def t_ID(t):
     r'[a-zA-Z][a-zA-Z_0-9]*'
     t.type = reserved.get(t.value, 'ID')
     return t
+
+t_EQUALS  = r'='
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+# t_POWER   = r'\^'
+t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_LT      = r'<'
+t_LE      = r'<='
+t_GT      = r'>'
+t_GE      = r'>='
+t_NE      = r'!='
+t_COMMA   = r'\,'
+t_SEMI    = r';'
+t_INTEGER = r'\d+'
+t_FLOAT   = r'((\d*\.\d+)(E[\+-]?\d+)?|([1-9]\d*E[\+-]?\d+))'
+t_STRING  = r'\".*?\"'
+
+t_ignore  = ' \t'
 
 def t_COMMENT(t):
     r'/\*.*\*/'
@@ -41,3 +62,7 @@ def t_COMMENT(t):
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
+
+def t_error(t):
+        print "Illegal character '%s'" % t.value[0]
+        t.lexer.skip(1)
